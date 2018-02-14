bf23da8633ff029b2bb0cbdd8d4790c3a93030d1
Jorge Blanco <jorge.blanco@babel.es>
2018-02-14 19:48:55 +0100

    Pues no lo era

--- ejer10.py
+++ ejer10.py
@@ -1,3 +1,5 @@
 import misfunciones
 
 misfunciones.alta_cliente('000000000','jorge','blanco','pais','de todos los santos','power','balance')
+
+print misfunciones.iniciales('jorge','blanco','pais','ole')
--- misfunciones.py
+++ misfunciones.py
@@ -13,3 +13,9 @@ def alta_cliente(dni,nombre,*apellidos):
 	for apellido in apellidos:
 		apellidos_completos += " " + apellido.capitalize() 
 	print nombre + apellidos_completos
+	
+def iniciales(nombre,*apellidos):
+	nombre_completo = nombre[0].upper()
+	for apellido in apellidos:
+		nombre_completo += "." + apellido[0].upper()
+	return nombre_completo
