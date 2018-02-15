d5f9ea430a92b0eb368a74dcc010a8f1be0a4019
root <root@localhost.localdomain>
2018-02-15 17:55:56 +0100

    Oh yeah nigga

--- librerias/ficheros.py
+++ librerias/ficheros.py
@@ -1,2 +1,8 @@
-def yoquese():
-	print "yo que se"
+import os
+
+def creadir(dir):
+	ruta = '/tmp/'+dir
+	if os.access(ruta,1):
+		return "Ya existe el directorio " + dir
+	os.mkdir(ruta)
+	return "He creado el directorio " + dir
